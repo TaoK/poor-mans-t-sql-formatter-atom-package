d8680ab5ad26d7e66d90fc5fbdcb2f15f1168de9
Tao Klerks <tao@klerks.biz>
2017-10-18 23:45:22 +0200

    Sample Package

---
+++ spec/poor-mans-t-sql-formatter-atom-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PoorMansTSqlFormatterAtomPackageView from '../lib/poor-mans-t-sql-formatter-atom-package-view';
+
+describe('PoorMansTSqlFormatterAtomPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
